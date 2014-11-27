66aa3d5edf96268f1f1e186aa67c678785c55b3b
Michiel Vandendriessche <michielvdd@me.com>
2014-11-27 14:51:18 +0100

    framing + nieuwe raw data

--- framer.py
+++ framer.py
@@ -1,10 +1,39 @@
-
+from frame import Frame
 
 class Framer:
     def __init__(self, frame_size, frame_overlap):
         self.frame_size = frame_size
         self.frame_overlap = frame_overlap
+        self.frames = list()
 
-    def frame(self, raw_data):
-        #TODO
-        print("TODO")
+    def get_frame_size(self):
+        return self.frame_size
+
+    def get_frame_overlap(self):
+        return self.frame_overlap
+
+    def get_frame_total_size(self):
+        return self.get_frame_size() + self.get_frame_overlap()*2
+
+    def get_frames(self):
+        return self.frames
+
+    def frame(self, data):
+
+        sample_counter = 0
+        sample_index = 0
+        frame = Frame()
+
+        while sample_index+self.get_frame_total_size() < len(data):
+
+            while sample_counter < self.get_frame_total_size():
+                if sample_index >= 0:
+                    frame.add_data(data[sample_index])
+                sample_counter += 1
+                sample_index += 1
+
+            self.get_frames().append(frame)
+            frame = Frame()
+            sample_counter = 0
+            sample_index -= 2*self.get_frame_overlap()
+
--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import sys
 from data_loader import *
-import matplotlib.pyplot as plt
+from framer import Framer
+"""import matplotlib.pyplot as plt"""
 
 def main(argv):
-    data_loader = DataLoader("data/train/")
-    data = data_loader.get_raw_data()
-    entry = data[1]
-    t = entry.get_time()
-    x = entry.get_x_axis()
-    y = entry.get_y_axis()
-    z = entry.get_z_axis()
-    plt.plot(t, x)
+    testDataLoader()
+    #testFramer()
+"""    plt.plot(t, x)
     plt.plot(t, y)
     plt.plot(t, z)
-    plt.show()
+    plt.show()"""
+
+def testFramer():
+    data = ["Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7", "Test8", "Test9", "Test10", "Test11", "Test12", "Test13", "Test14", "Test15", "Test16", "Test17", "Test18", "Test19", "Test20"]
+    framer = Framer(3, 2)
+    framer.frame(data)
+    frames = framer.get_frames()
+    for frame in frames:
+        print frame.get_data()
+
+def testFramer2(data):
+    framer = Framer(128, 64)
+    framer.frame(data)
+    frames = framer.get_frames()
+    i = 1
+    for frame in frames:
+        print "Frame number "+str(i)
+        i += 1
+        for data in frame.get_data():
+            print data.get_time()
+
+def testDataLoader():
+    data_loader = DataLoader("data/train/")
+    raw_data = data_loader.get_raw_data()
+    """for element in raw_data:
+        print "label: "+str(element.get_label())
+        print "time: "+str(element.get_time())
+        print "x: "+str(element.get_x())
+        print "y: "+str(element.get_y())
+        print "z: "+str(element.get_z())"""
+    testFramer2(raw_data)
+
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
