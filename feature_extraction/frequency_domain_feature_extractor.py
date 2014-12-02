b35306b0a4f5e5359d96587eb4103eee15620074
BavoGoosens <bavogoosens@hotmail.com>
2014-12-02 18:19:58 +0100

    features

--- feature_extraction/frequency_domain_feature_extractor.py
+++ feature_extraction/frequency_domain_feature_extractor.py
@@ -2,9 +2,44 @@ from feature_extractor import FeatureExtractor
 
 # TODO
 class FrequencyDomainFeatureExtractor(FeatureExtractor):
-
     def __init__(self):
         pass
 
     def extract_features(self):
+        pass
+
+    def calculate_linear_coefficients(self):
+        pass
+
+    def calculate_mel_scale_coeficients(self):
+        pass
+
+    def calculate_spectral_coefficients(self):
+        pass
+
+    def calculate_spectral_energy(self):
+        pass
+
+    def calculate_spectral_entropy(self):
+        pass
+
+    def calculate_ac_component(self):
+        pass
+
+    def calculate_dc_component(self):
+        pass
+
+    def calculate_mean(self):
+        pass
+
+    def calculate_correlation(self):
+        pass
+
+    def calculate_spectral_roll_off(self):
+        pass
+
+    def calculate_spectral_centeroid(self):
+        pass
+
+    def calculate_spectral_flux(self):
         pass
--- feature_extraction/time_domain_feature_extractor.py
+++ feature_extraction/time_domain_feature_extractor.py
@@ -2,8 +2,76 @@ from feature_extractor import FeatureExtractor
 
 # TODO
 class TimeDomainFeatureExtractor(FeatureExtractor):
-    def __init__(self):
-        pass
+    def __init__(self, frame):
+        self.frame = frame
+
+    def set_frame(self, frame):
+        self.frame = frame
 
     def extract_features(self):
+        self.calculate_mean()
+        self.calculate_integral()
+        self.calculate_kurtosis()
+
+    def calculate_mean(self):
+        pass
+
+    def calculate_integral(self):
+        pass
+
+    def calculate_kurtosis(self):
+        pass
+
+    def calculate_abs_mean(self):
+        pass
+
+    def calculate_variance(self):
+        pass
+
+    def calculate_minimum(self):
+        pass
+
+    def calculate_maximum(self):
+        pass
+
+    def calculate_abs_minimum(self):
+        pass
+
+    def calculate_abs_maximum(self):
+        pass
+
+    def calculate_avg_peak_distance(self):
+        pass
+
+    def calculate_peak_mean(self):
+        pass
+
+    def calculate_variance_in_peak(self):
+        pass
+
+    def calculate_apf(self):
+        pass
+
+    def calculate_variance_apf(self):
+        pass
+
+    def calculate_root_mean_square(self):
+        pass
+
+    def calculate_min_max_difference(self):
+        pass
+
+    def calculate_percentiles(self):
+        pass
+
+    def calculate_skewness(self):
+        pass
+
+    def calculate_median(self):
+        pass
+
+    def calculate_root_mean_square_of_integral(self):
+        pass
+
+    def calculate_mean_min_max_sums(self):
         pass
