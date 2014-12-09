a0202b1b0f4258b021b63b5183af48b49bcaf94d
BavoGoosens <bavogoosens@hotmail.com>
2014-12-09 21:32:30 +0100

    sklearn provision

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo apt-get install -y python-sklearn
